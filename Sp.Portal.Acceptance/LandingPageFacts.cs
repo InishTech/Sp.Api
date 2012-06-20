aad2e5906025920c15b35c76b05a91518cdfd070
Siobhan Connell <connell.siobhan@gmail.com>
2012-06-20 12:15:39 +0100

    Fix for incorrect expectation of guid string

--- Sp.Portal.Acceptance/LandingPageFacts.cs
+++ Sp.Portal.Acceptance/LandingPageFacts.cs
@@ -32,7 +32,7 @@
 			doc.LoadHtml( response.Content );
 			var node = doc.DocumentNode.SelectSingleNode( "//span[@data-claimid='customerid']" );
 			Assert.NotNull( node );
-			Assert.Contains( "BFF714F1-3C88-40E7-9E78-A73C041AC8EB", node.InnerText );
+			Assert.Contains( "bff714f1-3c88-40e7-9e78-a73c041ac8eb", node.InnerText );
 		}
 
 		[Theory, PortalData]
