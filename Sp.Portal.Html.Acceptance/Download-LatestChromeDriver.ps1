f370666e7b940c64ce4f7a31546bd6c1bf8211d4
Ruben Bartelink <rubenGithub@bartelink.com>
2012-07-04 16:10:39 +0100

    Automatic download/extract ChromeDriver.exe
    Like NuGet Package Restore, this removes the need for a ChromeDriver.exe
    to be retained in a bin or tools folder

---
+++ Sp.Portal.Html.Acceptance/Download-LatestChromeDriver.ps1
@@ -0,0 +1,19 @@
+﻿param( [string] $filename)
+#requires -version 3
+
+$url = "http://code.google.com/p/chromedriver/downloads/list"
+
+$result = Invoke-WebRequest $url 
+
+$href = $result.AllElements |
+    where TagName -eq "a" |
+    where href -match chromedriver_win_ |
+    where title -eq Download | 
+    select -ExpandProperty href |
+    select -first 1 
+
+$downloadUrl="http:$href"
+
+Write-Host "Downloading $downloadUrl to $filename"
+
+(New-Object System.Net.WebClient).DownloadFile( $downloadUrl, $filename)
---
+++ Sp.Portal.Html.Acceptance/Extract-Zip.ps1
@@ -0,0 +1,18 @@
+﻿param([string]$zipfilename, [string] $destination)
+
+$zipfilename=(Resolve-Path $zipfilename).Path
+
+if ( -not $( test-path($zipfilename))) {
+	throw "$zipfilename does not exist"
+}
+
+$destination=(Resolve-Path $destination).Path
+
+if ( -not $( test-path($destination))) {
+	throw "$destination does not exist"
+}
+
+$shellApplication = new-object -com shell.application
+$zipPackage = $shellApplication.NameSpace($zipfilename)
+$destinationFolder = $shellApplication.NameSpace($destination)
+$destinationFolder.CopyHere($zipPackage.Items())
