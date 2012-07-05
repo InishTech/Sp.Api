ff5fbddfa2556876321aca56cf125b3d96442d3c
Ruben Bartelink <rubenGithub@bartelink.com>
2012-07-05 16:56:02 +0100

    Review Code cleanup
    Added comments
    Added wrapping instead of try/finally

--- Sp.Portal.Html.Acceptance/BrowserDriverDataProvider.cs
+++ Sp.Portal.Html.Acceptance/BrowserDriverDataProvider.cs
@@ -1,15 +1,14 @@
-﻿using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium;
-using System.Configuration;
-
-namespace Sp.Portal.Html.Acceptance
+﻿namespace Sp.Portal.Html.Acceptance
 {
+	using OpenQA.Selenium;
+	using OpenQA.Selenium.Chrome;
+	using OpenQA.Selenium.Firefox;
+	using OpenQA.Selenium.Remote;
 	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Linq;
 
 	public class BrowserDriverDataProvider : SingleItemTheoryDataProvider
 	{
@@ -20,23 +19,9 @@ namespace Sp.Portal.Html.Acceptance
 				new ChromeDriver(),
 				new FirefoxDriver( GetSeleniumFirefoxProfile() )
 			};
-			foreach ( var driver in drivers )
-			{
-				Authenticate( driver );
-				yield return driver;
-			}
-		}
 
-		static FirefoxProfile GetSeleniumFirefoxProfile()
-		{
-			const string profileName = "Selenium";
-			FirefoxProfileManager profileManager = new FirefoxProfileManager();
-			FirefoxProfile profile = profileManager.GetProfile( profileName );
-			if ( profile == null )
-				throw new InvalidOperationException( "Cannot find Firefox profile " + profileName );
-			profile.SetPreference( "browser.ssl_override_behavior", 1 );
-			profile.AcceptUntrustedCertificates = false;
-			return profile;
+			return drivers
+				.Do( Authenticate );
 		}
 
 		static void Authenticate( RemoteWebDriver driver )
@@ -51,6 +36,61 @@ namespace Sp.Portal.Html.Acceptance
 
 			password.Submit();
 		}
+
+		static FirefoxProfile GetSeleniumFirefoxProfile()
+		{
+			const string profileName = "Selenium";
+			FirefoxProfileManager profileManager = new FirefoxProfileManager();
+			FirefoxProfile profile = profileManager.GetProfile( profileName );
+			if ( profile == null )
+				throw new InvalidOperationException( "Cannot find Firefox profile " + profileName );
+			profile.SetPreference( "browser.ssl_override_behavior", 1 );
+			profile.AcceptUntrustedCertificates = false;
+			return profile;
+		}
+	}
+
+	static class QuitGuardExtensions
+	{
+		public static IDisposable FinallyQuitGuard( this RemoteWebDriver that )
+		{
+			return new LambdaDisposable( that.Quit );
+		}
+
+		class LambdaDisposable : IDisposable
+		{
+			Action _action;
+
+			public LambdaDisposable( Action action )
+			{
+				_action = action;
+			}
+
+			void IDisposable.Dispose()
+			{
+				if ( _action == null )
+					throw new ObjectDisposedException( "Should not be triggering a LambdaDisposable twice" );
+
+				try
+				{
+					_action();
+				}
+				finally
+				{
+					_action = null;
+				}
+			}
+		}
+	}
+
+	public abstract class SingleItemTheoryDataProvider : TheoryDataProvider
+	{
+		protected override sealed IEnumerable<object[]> DataSource()
+		{
+			return SingleItemDataSource().Select( x => new[] { x } );
+		}
+
+		protected abstract IEnumerable<object> SingleItemDataSource();
 	}
 
 	public abstract class TheoryDataProvider : IEnumerable<object[]>
@@ -68,13 +108,15 @@ namespace Sp.Portal.Html.Acceptance
 		protected abstract IEnumerable<object[]> DataSource();
 	}
 
-	public abstract class SingleItemTheoryDataProvider : TheoryDataProvider
+	static class DoExtensions
 	{
-		protected override sealed IEnumerable<object[]> DataSource()
+		public static IEnumerable<T> Do<T>( this IEnumerable<T> thats, Action<T> action )
 		{
-			return SingleItemDataSource().Select( x => new[] { x } );
+			foreach ( var item in thats )
+			{
+				action( item );
+				yield return item;
+			}
 		}
-
-		protected abstract IEnumerable<object> SingleItemDataSource();
 	}
 }
--- Sp.Portal.Html.Acceptance/Licenses.cs
+++ Sp.Portal.Html.Acceptance/Licenses.cs
@@ -1,27 +1,27 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Configuration;
-using Xunit.Extensions;
-
-namespace Sp.Portal.Html.Acceptance
+﻿namespace Sp.Portal.Html.Acceptance
 {
-	public class Licenses
+	using OpenQA.Selenium;
+	using OpenQA.Selenium.Remote;
+	using OpenQA.Selenium.Support.UI;
+	using System;
+	using System.Configuration;
+	using Xunit.Extensions;
+
+	public class LicenseList
 	{
 		[Theory]
 		[ClassData( typeof( BrowserDriverDataProvider ) )]
-		public static void LicenseListShouldIncludeAtLeastOneLicense( RemoteWebDriver driver )
+		public static void ShouldIncludeAtLeastOneLicense( RemoteWebDriver driver )
 		{
-			try
+			using ( driver.FinallyQuitGuard() ) // TODO improve this using http://xunit.codeplex.com/workitem/9798 ( WAS: http://xunit.codeplex.com/discussions/362097 )
 			{
-				driver.Navigate().GoToUrl( ConfigurationManager.AppSettings[ "PortalBaseUrl" ] + "/license" );				
+				driver.Navigate().GoToUrl( ConfigurationManager.AppSettings[ "PortalBaseUrl" ] + "/license" );
+				// If we cannot respond in 5 seconds for any reason, a human will seriously distrust the software, no excuses
 				WebDriverWait wait = new WebDriverWait( driver, TimeSpan.FromSeconds( 5 ) );
-				wait.Until( ( d ) => { return d.FindElement( By.Id( "license-list" ) ).FindElements( By.TagName( "li" ) ).Count >= 1; } );
-			}
-			finally
-			{
-				driver.Quit();
+				wait.Until( d => d
+					.FindElement( By.Id( "license-list" ) )
+					.FindElements( By.TagName( "li" ) )
+					.Count > 0 );
 			}
 		}
 	}
