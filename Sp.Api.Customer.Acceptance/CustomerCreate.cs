9080088b64f0c074d2822fb350f8cadb760c224b
Siobhan Connell <connell.siobhan@gmail.com>
2012-07-24 15:24:33 +0100

    Added data validation tests to customer api test suite

--- Sp.Api.Customer.Acceptance/CustomerCreate.cs
+++ Sp.Api.Customer.Acceptance/CustomerCreate.cs
@@ -29,5 +29,51 @@ namespace Sp.Api.Customer.Acceptance
 			Verify.EventuallyWithBackOff( () =>
 					Assert.True( api.GetList().Data.Customers.Any( x => x.Name.Equals( newCustomerName ) ) ) );
 		}
+
+		/// <summary>
+		/// The following are examples of values that will be rejected as invalid.
+		/// - Description is Mandatory with a max length of 100 (empty is permitted).
+		/// - Label is Mandatory with a Minimum Length of 1 and max of 100.
+		/// </summary>
+		public static class InvalidData
+		{
+			public static class Name
+			{
+				[Theory, AutoSoftwarePotentialApiData]
+				public static void PutNullShouldReject( SpCustomerApi api )
+				{
+					var addLink = api.GetList().Data._links.add.href;
+					var response = api.AddCustomer( addLink, new SpCustomerApi.CustomerSummary() { Name = null, Description = "apiDescription" } );
+					Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+				}
+
+				[Theory, AutoSoftwarePotentialApiData]
+				public static void PutEmptyShouldReject( SpCustomerApi api )
+				{
+					var addLink = api.GetList().Data._links.add.href;
+					var response = api.AddCustomer( addLink, new SpCustomerApi.CustomerSummary() { Name = string.Empty, Description = "apiDescription" } );
+					Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+				}
+
+				[Theory, AutoSoftwarePotentialApiData]
+				public static void PutExcessivelyLongShouldReject( SpCustomerApi api )
+				{
+					var addLink = api.GetList().Data._links.add.href;
+					var response = api.AddCustomer( addLink, new SpCustomerApi.CustomerSummary() { Name = new String( 'a', 101 ), Description = "apiDescription" } );
+					Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+				}
+			}
+
+			public static class Description
+			{
+				[Theory, AutoSoftwarePotentialApiData]
+				public static void PutExcessivelyLongShouldReject( SpCustomerApi api )
+				{
+					var addLink = api.GetList().Data._links.add.href;
+					var response = api.AddCustomer( addLink, new SpCustomerApi.CustomerSummary() { Name = "apiName", Description = new String( 'a', 101 ) } );
+					Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+				}
+			}
+		}
 	}
 }
