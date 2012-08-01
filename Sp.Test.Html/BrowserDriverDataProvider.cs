5b559cf51b674c574e6ae031922cb65d0ce02564
Ruben Bartelink <rubenGithub@bartelink.com>
2012-08-01 13:51:37 +0100

    Reinstated running of ATs on Chrome

--- Sp.Test.Html/BrowserDriverDataProvider.cs
+++ Sp.Test.Html/BrowserDriverDataProvider.cs
@@ -19,10 +19,7 @@ namespace Sp.Test.Html
 		{
 			return new RemoteWebDriver[] 
 			{
-#if DEBUG
-				// TODO Temporarily inhibit on our Windows Server 2008 R2 test rig - TODO: reinstate
 				new ChromeDriver(),
-#endif
 				new FirefoxDriver( GetSeleniumFirefoxProfile() )
 			};
 		}
