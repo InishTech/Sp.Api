c411006b4857d7a112ce9b91acc84133a62f26b5
Ruben Bartelink <rubenGithub@bartelink.com>
2012-08-30 16:42:32 +0100

    Added CustomerTags Api & Acceptance

---
+++ Sp.Portal.Acceptance/CustomerTags.cs
@@ -0,0 +1,176 @@
+﻿namespace Sp.Portal.Acceptance
+{
+	using Ploeh.AutoFixture.Xunit;
+	using Sp.Portal.Acceptance.Wrappers;
+	using Sp.Test.Helpers;
+	using System;
+	using System.Linq;
+	using System.Net;
+	using Xunit;
+	using Xunit.Extensions;
+
+	public class CustomerTags
+	{
+		public class Index
+		{
+			public class Get
+			{
+				[Theory, PortalData]
+				public static void ShouldAlwaysBeAvailable( SpPortalApi api )
+				{
+					var response = api.GetTagCollection();
+					Assert.Equal( HttpStatusCode.OK, response.StatusCode );
+				}
+
+				[Theory, PortalData]
+				public static void ShouldHaveAddLink( SpPortalApi api )
+				{
+					var response = api.GetTagCollection();
+					Assert.Equal( HttpStatusCode.OK, response.StatusCode );
+
+					Assert.NotNull( response.Data._links );
+					Assert.NotNull( response.Data._links.add );
+					Assert.NotEmpty( response.Data._links.add.href );
+				}
+			}
+
+			public class Post
+			{
+				[Theory, PortalData]
+				public static void ShouldYieldAccepted( [Frozen]SpPortalApi api, AddLinkFixture addLink, Guid id, string name )
+				{
+					var apiResponse = api.AddTag( addLink.Href, id, name );
+					Assert.Equal( HttpStatusCode.Accepted, apiResponse.StatusCode );
+				}
+
+				[Theory, PortalData]
+				public static void ShouldEventuallyBeVisible( [Frozen]SpPortalApi api, ExistingTagFixture tag )
+				{
+					Verify.EventuallyWithBackOff( () =>
+					{
+						var apiResult = api.GetTagCollection();
+						Assert.Equal( HttpStatusCode.OK, apiResult.StatusCode );
+						Assert.True( apiResult.Data.Tags.Any( x => x.Name == tag.Name && tag.Href.Equals( x._links.self.href ) ) );
+					} );
+				}
+
+				[Theory, PortalData]
+				public static void DoubleShouldYieldConflict( [Frozen]SpPortalApi api, AddLinkFixture addLink, Guid id, string name )
+				{
+					var initialResponse = api.AddTag( addLink.Href, id, name );
+					Assert.Equal( HttpStatusCode.Accepted, initialResponse.StatusCode );
+					var response = api.AddTag( addLink.Href, id, name );
+					Assert.Equal( HttpStatusCode.Conflict, response.StatusCode );
+				}
+
+				public class Bad
+				{
+					[Theory, PortalData]
+					public static void MissingNameShouldYieldBadRequest( [Frozen]SpPortalApi api, AddLinkFixture addLink, Guid id )
+					{
+						var response = api.AddTag( addLink.Href, id, null );
+						Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+					}
+
+					[Theory( Skip = "Currently 500 instead due to choice of Serializer" ), PortalData]
+					public static void EmptyIdShouldYieldBadRequest( [Frozen]SpPortalApi api, AddLinkFixture addLink, string name )
+					{
+						var response = api.AddTag( addLink.Href, Guid.Empty, name );
+						Assert.Equal( HttpStatusCode.BadRequest, response.StatusCode );
+					}
+				}
+			}
+		}
+
+		public class Put
+		{
+			[Theory, PortalData]
+			public void ShouldYieldAccepted( [Frozen]SpPortalApi api, ExistingTagFixture tag, string newName )
+			{
+				using ( tag )
+				{
+					var response = api.RenameTag( tag.Href, newName );
+					Assert.Equal( HttpStatusCode.Accepted, response.StatusCode );
+				}
+			}
+
+			[Theory, PortalData]
+			public static void ShouldEventuallyBeVisible( [Frozen]SpPortalApi api, ExistingTagFixture tag, string newName )
+			{
+				using ( tag )
+				{
+					var response = api.RenameTag( tag.Href, newName );
+					Assert.Equal( HttpStatusCode.Accepted, response.StatusCode );
+
+					Verify.EventuallyWithBackOff( () =>
+					{
+						var apiResult = api.GetTagCollection();
+						Assert.Equal( HttpStatusCode.OK, apiResult.StatusCode );
+						Assert.True( apiResult.Data.Tags.Any( x => x.Name == newName && tag.Href.Equals( x._links.self.href ) ) );
+					} );
+				}
+			}
+		}
+
+		public class Delete
+		{
+			[Theory, PortalData]
+			public void ShouldYieldAccepted( [Frozen]SpPortalApi api, ExistingTagFixture tag )
+			{
+				var response = api.DeleteTag( tag.Href );
+				Assert.Equal( HttpStatusCode.Accepted, response.StatusCode );
+			}
+
+			[Theory, PortalData]
+			public static void ShouldEventuallyBeVisible( [Frozen]SpPortalApi api, ExistingTagFixture tag )
+			{
+				using ( tag )
+				{
+					var response = api.DeleteTag( tag.Href );
+					Assert.Equal( HttpStatusCode.Accepted, response.StatusCode );
+
+					Verify.EventuallyWithBackOff( () =>
+					{
+						var apiResult = api.GetTagCollection();
+						Assert.Equal( HttpStatusCode.OK, apiResult.StatusCode );
+						Assert.False( apiResult.Data.Tags.Any( x => tag.Href.Equals( x._links.self.href ) ) );
+					} );
+				}
+			}
+		}
+
+		public class ExistingTagFixture : IDisposable
+		{
+			readonly SpPortalApi _api;
+			public string Href { get; private set; }
+			public string Name { get; private set; }
+
+			public ExistingTagFixture( AddLinkFixture addLink, SpPortalApi api, Guid id, string name )
+			{
+				Name = name;
+				_api = api;
+				var response = api.AddTag( addLink.Href, id, name );
+				Assert.Equal( HttpStatusCode.Accepted, response.StatusCode );
+				Href = Assert.IsType<string>( response.Headers.Single( x => x.Name == "Location" ).Value );
+			}
+
+			void IDisposable.Dispose()
+			{
+				_api.DeleteTag( Href );
+			}
+		}
+
+		public class AddLinkFixture
+		{
+			public string Href { get; private set; }
+
+			public AddLinkFixture( SpPortalApi api )
+			{
+				var response = api.GetTagCollection();
+				Assert.Equal( HttpStatusCode.OK, response.StatusCode );
+				Href = response.Data._links.add.href;
+				Assert.NotEmpty( Href );
+			}
+		}
+	}
+}
--- Sp.Portal.Acceptance/Wrappers/SpPortalApi.cs
+++ Sp.Portal.Acceptance/Wrappers/SpPortalApi.cs
@@ -8,6 +8,8 @@ namespace Sp.Portal.Acceptance.Wrappers
 	using RestSharp;
 	using Sp.Api.Shared.Wrappers;
 	using Sp.Test.Helpers;
+	using System;
+	using System.Collections.Generic;
 
 	public class SpPortalApi
 	{
@@ -18,13 +20,77 @@ namespace Sp.Portal.Acceptance.Wrappers
 		public SpPortalApi( SpApiConfiguration apiConfiguration )
 		{
 			_client = new RelativePathAwareCustomRestClient( apiConfiguration.BaseUrl );
-			
+
 			_username = apiConfiguration.Username;
 			_password = apiConfiguration.Password;
 
 			_client.Authenticator = new WSFederationAuthenticator( _username, _password );
 		}
 
+		internal IRestResponse<TagCollection> GetTagCollection()
+		{
+			var request = new RestRequest( "tag" );
+			return Execute<TagCollection>( request );
+		}
+
+		public IRestResponse AddTag( string addLinkHref, Guid requestId, string name )
+		{
+			var request = new RestRequest( addLinkHref, Method.POST );
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody( new TagCreateRequest { RequestId = requestId, Name = name } );
+			return Execute( request );
+		}
+
+		public IRestResponse RenameTag( string tagHref, string newName )
+		{
+			var request = new RestRequest( tagHref, Method.PUT );
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody( new Tag { Name = newName } );
+			return Execute( request );
+		}
+
+		public IRestResponse DeleteTag( string tagHref )
+		{
+			var request = new RestRequest( tagHref, Method.DELETE );
+			return Execute( request );
+		}
+
+		public class TagCollection
+		{
+			public List<Tag> Tags { get; set; }
+
+			public Links _links { get; set; }
+
+			public class Links
+			{
+				public Link add { get; set; }
+			}
+		}
+
+		public class Tag
+		{
+			public string Name { get; set; }
+
+			public Links _links { get; set; }
+
+			public class Links
+			{
+				public Link self { get; set; }
+			}
+		}
+
+		public class TagCreateRequest
+		{
+			public Guid RequestId { get; set; }
+			public string Name { get; set; }
+		}
+
+		public class Link
+		{
+			public string href { get; set; }
+		}
+
+		#region Ignore
 		public IRestResponse<T> Execute<T>( RestRequest request ) where T : new()
 		{
 			return _client.Execute<T>( request );
@@ -40,5 +106,6 @@ namespace Sp.Portal.Acceptance.Wrappers
 			var signOffRequest = new RestRequest( "Authentication/LogOff", Method.GET );
 			return _client.Execute( signOffRequest );
 		}
+		#endregion
 	}
 }
