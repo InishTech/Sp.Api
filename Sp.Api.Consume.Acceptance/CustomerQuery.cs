ac7eb8d1e1d7d76f8674e5be1267191004737caf
Siobhan Connell <connell.siobhan@gmail.com>
2013-04-05 15:38:11 +0100

    Test demonstrating oData filtering on externalId

---
+++ Sp.Api.Consume.Acceptance/CustomerQuery.cs
@@ -0,0 +1,19 @@
+﻿using Sp.Api.Shared;
+using System.Net;
+using Xunit;
+using Xunit.Extensions;
+using System.Linq;
+
+namespace Sp.Api.Consume.Acceptance
+{
+	public static class CustomerQuery
+	{
+		[Theory, AutoSoftwarePotentialApiData]
+		public static void FilterByKnownExternalIdShouldReturnOneOrMoreCustomersWithThatExternalId( SpCustomerApi api, RandomCustomerFromListFixture preSelectedCustomer )
+		{
+			var response = api.GetCustomerList( "$filter=substringof('" + preSelectedCustomer.Selected.ExternalId + "', ExternalId) eq true" );
+			Assert.NotNull( response.Data.results );
+			Assert.False( response.Data.results.Any( x => x.ExternalId != preSelectedCustomer.Selected.ExternalId ) );
+		}
+	}
+}
