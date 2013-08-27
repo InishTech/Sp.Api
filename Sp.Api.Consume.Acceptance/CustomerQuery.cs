dcd8dc26420efb440da817fe2a7adc36d9e7c667
Ruben Bartelink <ruben@bartelink.com>
2013-08-27 16:25:03 +0100

    Reversed incorrect condition
    Was unintentional typo- fixed by making test positive rather than
    negative

--- Sp.Api.Consume.Acceptance/CustomerQuery.cs
+++ Sp.Api.Consume.Acceptance/CustomerQuery.cs
@@ -14,7 +14,7 @@ namespace Sp.Api.Consume.Acceptance
 		{
 			var response = api.GetCustomerList( "$filter=ExternalId eq '" + preSelectedCustomer.Selected.ExternalId + '"' );
 			Assert.NotNull( response.Data.results );
-			Assert.False( response.Data.results.Any( x => String.Equals( x.ExternalId, preSelectedCustomer.Selected.ExternalId, StringComparison.OrdinalIgnoreCase /* String field in database will be case-insentistive due to collation configuration */) ) );
+			Assert.True( response.Data.results.All( x => String.Equals( x.ExternalId, preSelectedCustomer.Selected.ExternalId, StringComparison.OrdinalIgnoreCase /* String field in database will be case-insentistive due to collation configuration */) ) );
 		}
 	}
 }
