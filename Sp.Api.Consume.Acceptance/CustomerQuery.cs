5f9efd14ae698846afc9081ac7698b930f61d5ba
Ruben Bartelink <ruben@bartelink.com>
2013-08-28 15:44:24 +0100

    Added diagnostic info to Customer Search
    Repeatedly failing test has not responded to random improvements to date
    so now gathering evidence for a more targeted attack

--- Sp.Api.Consume.Acceptance/CustomerQuery.cs
+++ Sp.Api.Consume.Acceptance/CustomerQuery.cs
@@ -1,20 +1,18 @@
 ﻿using Sp.Api.Shared;
-using System.Net;
+using System.Linq;
 using Xunit;
 using Xunit.Extensions;
-using System.Linq;
-using System;
 
 namespace Sp.Api.Consume.Acceptance
 {
 	public static class CustomerQuery
 	{
 		[Theory, AutoSoftwarePotentialApiData]
-		public static void FilterByKnownExternalIdShouldReturnOneOrMoreCustomersWithThatExternalId( SpCustomerApi api, RandomCustomerFromListFixture preSelectedCustomer )
+		static void FilterByKnownExternalIdShouldReturnOneOrMoreCustomersWithThatExternalId( SpCustomerApi api, RandomCustomerFromListFixture preSelectedCustomer )
 		{
-			var response = api.GetCustomerList( "$filter=ExternalId eq '" + preSelectedCustomer.Selected.ExternalId + '"' );
-			Assert.NotNull( response.Data.results );
-			Assert.True( response.Data.results.All( x => String.Equals( x.ExternalId, preSelectedCustomer.Selected.ExternalId, StringComparison.OrdinalIgnoreCase /* String field in database will be case-insentistive due to collation configuration */) ) );
+			var id = preSelectedCustomer.Selected.ExternalId;
+			var response = api.GetCustomerList( "$filter=ExternalId eq '" + id + '"' );
+			Assert.True( response.Data.results.All( x => id == x.ExternalId ), "Did not find '" + id + "' in: " + string.Join( ", ", from r in response.Data.results select r.ExternalId ) );
 		}
 	}
 }
