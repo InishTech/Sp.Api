130643dc05cad062297f0e2b07c95ad195594812
Adam Jasinski <ajasinski@inishtech.com>
2013-10-08 10:18:59 +0100

    Download-GoogleCodeDownload.ps1 can now download Selenium drivers from Google Cloud Storage

--- Sp.Test.Html/Download-GoogleCodeDownload.ps1
+++ Sp.Test.Html/Download-GoogleCodeDownload.ps1
@@ -3,20 +3,35 @@
 # This code is licensed under the BSD 3-Clause License included with this source
 # 
 # FOR DETAILS, SEE https://github.com/InishTech/Sp.Api/wiki/License 
-param( [string] $filename, [string] $namePrefix, [string] $url )
+param( [string] $filename, [string] $namePrefix, [string] $url, [string] $indexPageType )
 
 #requires -version 3
+$ErrorActionPreference = "Stop"
+
+function extractDownloadUrlFromGoogleCodeIndexPage($pageResult){
+	$href = $pageResult.AllElements |
+          where TagName -eq "a" |
+          where href -match $namePrefix |
+          where title -eq Download | 
+          select -ExpandProperty href |
+          select -first 1 
+    return "http:$href"
+}
+
+function extractDownloadUrlFromGoogleCloudStorageIndexPage($pageResult){
+	[xml] $releaseIndex = $pageResult
+	$lastRelease = $releaseIndex.ListBucketResult.Contents | where { $_.Key.Contains($namePrefix) } | sort Key -desc | select -first 1
+	return "$url/$($lastRelease.Key)"
+}
 
 $result = Invoke-WebRequest $url 
 
-$href = $result.AllElements |
-    where TagName -eq "a" |
-    where href -match $namePrefix |
-    where title -eq Download | 
-    select -ExpandProperty href |
-    select -first 1 
-
-$downloadUrl="http:$href"
+if( $indexPageType -eq "GoogleCloudStorage"){
+	$downloadUrl = extractDownloadUrlFromGoogleCloudStorageIndexPage $result
+}
+else {
+	$downloadUrl = extractDownloadUrlFromGoogleCodeIndexPage $result
+}
 
 Write-Host "Downloading $downloadUrl to $filename"
 
