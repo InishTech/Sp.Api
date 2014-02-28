ebf61d9fbf8e294604d822230bf96995b1772255
adamjasinski <ajasinski@inishtech.com>
2014-02-28 15:05:31 +0000

    Updated IEDriver download URL
    deleted unused code in GoogleCodeDownload.ps1

--- Sp.Test.Html/Download-GoogleCodeDownload.ps1
+++ Sp.Test.Html/Download-GoogleCodeDownload.ps1
@@ -3,20 +3,10 @@
 # This code is licensed under the BSD 3-Clause License included with this source
 # 
 # FOR DETAILS, SEE https://github.com/InishTech/Sp.Api/wiki/License 
-param( [string] $filename, [string] $namePrefix, [string] $url, [string] $indexPageType )
+param( [string] $filename, [string] $namePrefix, [string] $url )
 
 #requires -version 3
 $ErrorActionPreference = "Stop"
-
-function extractDownloadUrlFromGoogleCodeIndexPage($pageResult){
-	$href = $pageResult.AllElements |
-          where TagName -eq "a" |
-          where href -match $namePrefix |
-          where title -eq Download | 
-          select -ExpandProperty href |
-          select -first 1 
-    return "http:$href"
-}
 
 function extractDownloadUrlFromGoogleCloudStorageIndexPage($pageResult){
 	[xml] $releaseIndex = $pageResult
@@ -24,14 +14,8 @@ function extractDownloadUrlFromGoogleCloudStorageIndexPage($pageResult){
 	return "$url/$($lastRelease.Key)"
 }
 
-$result = Invoke-WebRequest $url 
-
-if( $indexPageType -eq "GoogleCloudStorage"){
-	$downloadUrl = extractDownloadUrlFromGoogleCloudStorageIndexPage $result
-}
-else {
-	$downloadUrl = extractDownloadUrlFromGoogleCodeIndexPage $result
-}
+$result = Invoke-WebRequest $url
+$downloadUrl = extractDownloadUrlFromGoogleCloudStorageIndexPage $result
 
 Write-Host "Downloading $downloadUrl to $filename"
 
