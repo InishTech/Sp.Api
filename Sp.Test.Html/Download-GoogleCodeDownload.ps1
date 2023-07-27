e33411def2d7169d740d5f305c3a62401ddbf944
nboon <nboon@inishtech.com>
2023-07-27 16:52:52 +0100

    Updating ChromeDriver download.

--- Sp.Test.Html/Download-GoogleCodeDownload.ps1
+++ Sp.Test.Html/Download-GoogleCodeDownload.ps1
@@ -3,23 +3,30 @@
 # This code is licensed under the BSD 3-Clause License included with this source
 # 
 # FOR DETAILS, SEE https://github.com/InishTech/Sp.Api/wiki/License 
-param( [string] $filename, [string] $namePrefix, [string] $url )
+param( [string] $path, [string] $zipFilename, [string] $os )
 
 #requires -version 3
 $ErrorActionPreference = "Stop"
+$chromeDriverVersionPath = "$path/chromeDriverVersion.txt"
+$zipPath = "$path\$zipFilename"
+$versionUrl = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json"
+$lastKnownGoodVersions = Invoke-RestMethod $versionUrl
+$currentStable = $lastKnownGoodVersions.channels.stable.version
 
-function extractDownloadUrlFromGoogleCloudStorageIndexPage($pageResult, $release) {
-    [xml] $releaseIndex = $pageResult
-    $file = $releaseIndex.ListBucketResult.Contents | Where-Object { $_.Key.Contains($namePrefix) } | Where-Object { $_.Key.Contains($release) }
-    return 	"$url/$($file.Key)"
+if(Test-Path $chromeDriverVersionPath) {
+	$chromeDriverVersion = Get-Content -Path $chromeDriverVersionPath
 }
 
-$result = Invoke-WebRequest $url
-$chromeMajorVersion = ((Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo).ProductVersion.split('.')[0];
-$latestRelease = Invoke-WebRequest "$url/LATEST_RELEASE_$chromeMajorVersion"
-$downloadUrl = extractDownloadUrlFromGoogleCloudStorageIndexPage $result $latestRelease
+if($chromeDriverVersion -eq $currentStable) {
+	Write-Host "ChromeDriver is up to date at $chromeDriverVersion. Skipping download."
+	return
+}
 
-Write-Host "Downloading $downloadUrl to $filename"
+New-Item -Path $chromeDriverVersionPath -ItemType "file" -Value "$currentStable" -Force
 
-(New-Object System.Net.WebClient).DownloadFile( $downloadUrl, $filename)
-Unblock-File $filename
+$downloadUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$currentStable/$os/chromedriver-$os.zip"
+Write-Host "Downloading $downloadUrl to $zipPath"
+(New-Object System.Net.WebClient).DownloadFile( $downloadUrl, $zipPath)
+Unblock-File $zipPath
+
+& $PSScriptRoot\Extract-Zip.ps1 $zipPath $path;
