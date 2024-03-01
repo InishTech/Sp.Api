c48236745d80b3dc106ee28504afdd1de23cbfef
nboon <nboon@inishtech.com>
2024-03-01 14:41:49 +0000

    #487 Technical: Retrieving url for chromedriver download from API

--- Sp.Test.Html/Download-GoogleCodeDownload.ps1
+++ Sp.Test.Html/Download-GoogleCodeDownload.ps1
@@ -9,22 +9,25 @@ param( [string] $path, [string] $zipFilename, [string] $os )
 $ErrorActionPreference = "Stop"
 $chromeDriverVersionPath = "$path/chromeDriverVersion.txt"
 $zipPath = "$path\$zipFilename"
-$versionUrl = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json"
+$versionUrl = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json"
 $lastKnownGoodVersions = Invoke-RestMethod $versionUrl
-$currentStable = $lastKnownGoodVersions.channels.stable.version
+$currentStable = $lastKnownGoodVersions.channels.stable
+$version = $currentstable.version
+$platformObject = $currentStable.downloads.chromedriver | Where-Object {$_.platform -eq $os} |  Select-Object -First 1
+$downloadUrl = $platformObject.url;
+
+Write-Host $downloadUrl
 
 if(Test-Path $chromeDriverVersionPath) {
 	$chromeDriverVersion = Get-Content -Path $chromeDriverVersionPath
 }
 
-if($chromeDriverVersion -eq $currentStable) {
+if($chromeDriverVersion -eq $version) {
 	Write-Host "ChromeDriver is up to date at $chromeDriverVersion. Skipping download."
 	return
 }
 
-New-Item -Path $chromeDriverVersionPath -ItemType "file" -Value "$currentStable" -Force
-
-$downloadUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$currentStable/$os/chromedriver-$os.zip"
+New-Item -Path $chromeDriverVersionPath -ItemType "file" -Value "$version" -Force
 Write-Host "Downloading $downloadUrl to $zipPath"
 (New-Object System.Net.WebClient).DownloadFile( $downloadUrl, $zipPath)
 Unblock-File $zipPath
